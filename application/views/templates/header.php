b59b624efd60377f4c1e29497a45855155bce116
snailpjw <alanwei1202@gmail.com>
2017-03-23 19:53:48 +0800

    add folder template

---
+++ application/views/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html data-ts-native>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Script-->
+    <script src="https://unpkg.com/vue/dist/vue.js"></script>
+    <!--Style-->
+
+    <!--Title-->
+    <title>
+        <?php echo $title ?>
+    </title>
+
+</head>
+<body>
