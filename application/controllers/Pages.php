8fa88fc1fd2cec1829bea8218a1c5f10168ac73b
snailpjw <alanwei1202@gmail.com>
2017-03-23 22:26:26 +0800

    update Pages.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -10,17 +10,17 @@ class Pages extends CI_Controller
         // $this->load->model('SiteMsgModel', 'siteMsgModel');
     }
     private function getPageToTitle(){
-        return array(
-            'account-settings'=>'帳戶設定',
-            'search-tutorial'=>'尋找教學課程',
-            'register-account'=>'註冊',
-            'raise-fund-for-tutorial'=>'募資中課程',
-            'start-new-tutorial'=>'我要開新課程',
-            'login'=>'登入/註冊',
-            'authorize-pending-tutorials'=>'核准新課程',
-            'customer-service'=>'客戶服務',
-            'answer-ticket'=>'回覆客訴',
-            'on-site-msg'=>'站內訊息'
-        );
+        // return array(
+        //     'account-settings'=>'帳戶設定',
+        //     'search-tutorial'=>'尋找教學課程',
+        //     'register-account'=>'註冊',
+        //     'raise-fund-for-tutorial'=>'募資中課程',
+        //     'start-new-tutorial'=>'我要開新課程',
+        //     'login'=>'登入/註冊',
+        //     'authorize-pending-tutorials'=>'核准新課程',
+        //     'customer-service'=>'客戶服務',
+        //     'answer-ticket'=>'回覆客訴',
+        //     'on-site-msg'=>'站內訊息'
+        // );
     }
 }
